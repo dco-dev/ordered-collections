d83c802b175f41684aee642b9421b179d102dfed
Dan Lentz <danlentz@gmail.com>
2026-03-14 19:51:00 -0400

    test: merge-withs

--- test/com/dean/ordered_collections/ordered_map_test.clj
+++ test/com/dean/ordered_collections/ordered_map_test.clj
@@ -1,6 +1,11 @@
 (ns com.dean.ordered-collections.ordered-map-test
   (:require [clojure.test                :refer :all]
-            [com.dean.ordered-collections.core :refer [ordered-map ordered-map-by assoc-new]])
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [com.dean.ordered-collections.core :refer [ordered-map ordered-map-by
+                                                       ordered-merge-with assoc-new]]
+            [com.dean.ordered-collections.test-utils :as tu])
   (:import  [java.util UUID]))
 
 
@@ -72,3 +77,66 @@
     (let [m (ordered-map [[1 :a] [3 :c] [5 :e]])]
       (is (= [[1 :a] [2 :b] [3 :c] [5 :e]]
              (seq (assoc-new m 2 :b)))))))
+
+(deftest ordered-merge-with-test
+  (testing "non-overlapping keys — simple union"
+    (let [m1 (ordered-map [[1 :a] [3 :c]])
+          m2 (ordered-map [[2 :b] [4 :d]])]
+      (is (= {1 :a 2 :b 3 :c 4 :d}
+             (ordered-merge-with (fn [k a b] a) m1 m2)))))
+
+  (testing "overlapping keys — merge function called with correct argument order"
+    (let [m1     (ordered-map [[1 10] [2 20] [3 30]])
+          m2     (ordered-map [[2 200] [3 300] [4 400]])
+          calls  (atom [])
+          result (ordered-merge-with
+                   (fn [k existing incoming]
+                     (swap! calls conj [k existing incoming])
+                     (+ existing incoming))
+                   m1 m2)]
+      ;; merge-fn receives (key, val-in-result, val-in-latter)
+      (is (= {1 10, 2 220, 3 330, 4 400} result))
+      (is (some #(= [2 20 200] %) @calls))
+      (is (some #(= [3 30 300] %) @calls))))
+
+  (testing "last-wins semantics"
+    (let [m1 (ordered-map [[1 :first] [2 :first]])
+          m2 (ordered-map [[2 :second] [3 :second]])
+          m3 (ordered-map [[3 :third] [4 :third]])]
+      (is (= {1 :first, 2 :second, 3 :third, 4 :third}
+             (ordered-merge-with (fn [k a b] b) m1 m2 m3)))))
+
+  (testing "first-wins semantics"
+    (let [m1 (ordered-map [[1 :first] [2 :first]])
+          m2 (ordered-map [[2 :second] [3 :second]])
+          m3 (ordered-map [[3 :third] [4 :third]])]
+      (is (= {1 :first, 2 :first, 3 :second, 4 :third}
+             (ordered-merge-with (fn [k a b] a) m1 m2 m3)))))
+
+  (testing "empty maps"
+    (is (nil? (ordered-merge-with (fn [k a b] b))))
+    (is (nil? (ordered-merge-with (fn [k a b] b) nil nil)))
+    (let [m (ordered-map [[1 :a]])]
+      (is (= m (ordered-merge-with (fn [k a b] b) m nil)))
+      (is (= m (ordered-merge-with (fn [k a b] b) nil m)))))
+
+  (testing "single map"
+    (let [m (ordered-map [[1 :a] [2 :b]])]
+      (is (= m (ordered-merge-with (fn [k a b] b) m))))))
+
+(defspec prop-merge-with-addition-is-commutative 100
+  (prop/for-all [kvs1 tu/gen-int-map-entries
+                 kvs2 tu/gen-int-map-entries]
+    (let [m1 (tu/->om kvs1)
+          m2 (tu/->om kvs2)
+          add (fn [k a b] (+ a b))]
+      (= (ordered-merge-with add m1 m2)
+         (ordered-merge-with add m2 m1)))))
+
+(defspec prop-merge-with-last-wins-equals-merge 100
+  (prop/for-all [kvs1 tu/gen-int-map-entries
+                 kvs2 tu/gen-int-map-entries]
+    (let [m1 (tu/->om kvs1)
+          m2 (tu/->om kvs2)]
+      (= (into m1 m2)
+         (ordered-merge-with (fn [k a b] b) m1 m2)))))
